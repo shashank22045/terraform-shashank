f7b4bb24906b617ecefd07df173ac5765c01231e
shashank22045 <53117371+shashank22045@users.noreply.github.com>
2022-12-29 21:11:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
-  profile = "terraform"
+    access_key = "AKIA2TOUPOPAIK77MPGH"
+  secret_key = "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"
+  region     = "ca-central-1"
 }
